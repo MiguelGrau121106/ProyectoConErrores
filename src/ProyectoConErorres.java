00c235dd8733a27cf2425fd57b20813396be179e
Miguel Grau <miggrafer@alu.edu.gva.es>
2025-04-14 11:37:25 +0200

    Proyecto con errores creado

---
+++ src/ProyectoConErorres.java
@@ -0,0 +1,29 @@
+
+
+public class ProyectoConErorres {
+
+
+
+        public static void main(String[] args) {
+            List<String> nombres = new ArrayList<>();
+
+            imprimirNombres(nombres);
+        }
+
+        public static imprimirNombres(List<String> lista) {
+            for (String nombre : lista) {
+                System.out.println(nombreCompleto);
+            }
+
+            Persona p = new Persona("Juan", "Pérez");
+        }
+    }
+
+    class Persona {
+        private String nombre;
+
+        public Persona() {
+            this.nombre = "Sin nombre";
+        }
+    }
+}
